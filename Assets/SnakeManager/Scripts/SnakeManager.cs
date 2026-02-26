6acc0b6425d2d3684f0de45eb63f45301dfdebb6
wtr <w.inou.24.8.1574@4nonome.com>
2026-02-27 04:50:17 +0900

    add SnakeManager.cs
    SnakeManagerでは胴体を増やすための機能を追加した。

--- Assets/SnakeManager/Scripts/SnakeManager.cs
+++ Assets/SnakeManager/Scripts/SnakeManager.cs
@@ -6,6 +6,10 @@ public class SnakeManager : MonoBehaviour
 {
     // シングルトンインスタンス
     public static SnakeManager Instance { get; private set; }
+    public GameObject snakeHeadObject;
+    public GameObject bodyPrefab;
+    private ISnake snakeHead;
+    private ISnake snakeTail;
     // Start is called before the first frame update
     void Awake()
     {
@@ -18,5 +22,32 @@ public class SnakeManager : MonoBehaviour
             Destroy(gameObject);
         }
         DontDestroyOnLoad(gameObject);
+        snakeHead = snakeHeadObject.GetComponent<ISnake>();
+        SetSnakeTail(snakeHead);
+    }
+
+    public void SetSnakeTail(ISnake snakeTail)
+    {
+        this.snakeTail = snakeTail;
+    }
+
+    public ISnake GetSnakeTail()
+    {
+        return snakeTail;
+    }
+
+    void GenerateBody(ISnake frontSnake)
+    {
+        GameObject bodyObj = Instantiate(bodyPrefab);
+
+        BodyBehaviour body = bodyObj.GetComponent<BodyBehaviour>();
+
+        if (body == null)
+        {
+            Debug.LogError("BodyBehaviour が付いていません");
+            return;
+        }
+
+        body.SetFrontSnake(frontSnake);
     }
 }
