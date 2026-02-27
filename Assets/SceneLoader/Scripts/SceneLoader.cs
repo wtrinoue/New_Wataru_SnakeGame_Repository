e518c2fe2c61fa9f017cf4528b1b83d37dd0dd91
wtr <w.inou.24.8.1574@4nonome.com>
2026-02-28 07:21:20 +0900

    add TitleScene
    Startボタンが配置されたタイトルとなるシーンを追加した。Startボタンを押すとプレイ画面に移動する。

---
+++ Assets/SceneLoader/Scripts/SceneLoader.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneLoader : MonoBehaviour
+{
+    // シングルトンインスタンス
+    public static SceneLoader Instance { get; private set; }
+
+    void Awake()
+    {
+        if (Instance == null)
+        {
+            Instance = this;
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+        DontDestroyOnLoad(gameObject);
+    }
+
+    public void SceneLoad(string sceneName)
+    {
+        SceneManager.LoadScene(sceneName);
+    }
+}
